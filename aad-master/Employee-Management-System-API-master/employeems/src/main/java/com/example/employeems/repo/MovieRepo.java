28edc2763919dd28e9f0ec2a0beca20e37a1fdde
Anjana <sanjudilka5@gmail.com>
2025-04-22 02:20:56 +0530

    2

--- aad-master/Employee-Management-System-API-master/employeems/src/main/java/com/example/employeems/repo/MovieRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.employeems.repo;
-
-import com.example.employeems.entity.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepo extends JpaRepository<Movie, Integer> {}
-
